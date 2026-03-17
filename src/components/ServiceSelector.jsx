9c1f324374db11c4b7a3736c69739e864bfcff72
Evelyn <eve.sepulveda@hotmail.es>
2026-03-17 12:43:47 -0300

    fix duplicate React import

--- src/components/ServiceSelector.jsx
+++ src/components/ServiceSelector.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Beer, Info, ShoppingCart, Users, Truck, Check } from 'lucide-react';
